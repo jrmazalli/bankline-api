818dc3815fc8c10de9c3d3ec15771aaf8e29dfec
Junior Mazalli <junior.mazalli@afgbrasil.com.br>
2022-05-04 14:35:44 -0300

    ajustes

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.model.Movimentacao;
 import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
 
 @Service
@@ -15,6 +17,9 @@ public class MovimentacaoService {
 
 	@Autowired
 	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
 
 	public void save(NovaMovimentacao novaMovimentacao) {
 
@@ -29,6 +34,12 @@ public class MovimentacaoService {
 		movimentacao.setTipo(novaMovimentacao.getTipo());
 		movimentacao.setValor(valor);
 
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if (correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
+		
 		repository.save(movimentacao);
 	}
 
